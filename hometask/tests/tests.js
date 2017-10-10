88d007be46370297a2eea04a7102018462b01d39
br3t <bret.snk@gmail.com>
2017-10-10 17:58:50 +0300

    More fixes for tests

--- hometask/tests/tests.js
+++ hometask/tests/tests.js
@@ -6,6 +6,7 @@ testManager.init = function() {
 	ctx.fillSelect();
 
 	$('body').on('change', '#tests-list', function() {
+		ctx.clearMessages();
 		if($(this).val()) {
 			var selectedTask = $(this).val().split(':');
 			ctx.getSelectedTests(selectedTask);
@@ -13,11 +14,9 @@ testManager.init = function() {
 	});
 
 	$('body').on('click', '#run-tests', function() {
-
+		ctx.runTests();
 	});
 };
-
-
 
 testManager.fillSelect = function() {
 	var select = $('#tests-list');
@@ -36,25 +35,103 @@ testManager.getSelectedTests = function(selectedTask) {
 	$( "#task-info" ).load(url);
 
 	var selectedLessonData = testsData.find(function(v) { return v.folder == selectedTask[0]; });
-	ctx.selectedTaskData = selectedLessonData.tasks.find(function(v) { return v.id == selectedTask[1]; });
+	this.selectedTaskData = selectedLessonData.tasks.find(function(v) { return v.id == selectedTask[1]; });
 };
 
 testManager.runTests = function() {
+	var ctx = this;
 	var result = '';
+	var counter = 1;
 	var functionCode = $('#task-solution').val();
-	eval(functionCode);
-	this.selectedTaskData.tests.forEach(function(v) {
-		var testResult = this.proxy(this.selectedTaskData.functionName, v.input);
-		if(this.isSimilar(testResult, v.output)) {
-			console.log('Ok');
-		} else {
-			console.log(':(');
+	ctx.clearMessages();
+	if(functionCode == '' || functionCode.indexOf('function') == -1) {
+		ctx.addMessage('error', 'Добавьте код функции');
+		return;
+	}
+	try {
+		window[ctx.selectedTaskData.functionName] = eval('(' + functionCode + ')');
+	} catch(e) {
+		ctx.addMessage('error', e.message);
+		return;
+	}
+	
+	ctx.selectedTaskData.tests.forEach(function(v) {
+		var testResult = ctx.proxy(ctx.selectedTaskData.functionName, v.input);
+		switch(ctx.isSimilar(testResult, v.output)) {
+			case 2:
+				ctx.addMessage('ok', 'Тест ' + counter + ' пройден');
+			break;
+			case 1:
+				ctx.addMessage('ok', 'Тест ' + counter + ' пройден, но имеется ошибка в типе результата');
+			break;
+			default:
+				ctx.addMessage('error', 'Тест ' + counter + ' провален' +
+					'<div><b>Ввод:</b> ' + v.input + '<br />' +
+					'<b>Вывод:</b> ' + testResult + '<br />' +
+					'<b>Ожидаемый вывод:</b> ' + v.output);
+			break;
 		}
+		counter++;
 	});
 };
 
-testManager.isSimilar = function() {
+testManager.proxy = function(functionName, input) {
+	var result = null;
+	if(functionName in window) {
+		switch(input.length) {
+			case 0:
+				result = window[functionName]();
+			break;
+			case 1:
+				result = window[functionName](input[0]);
+			break;
+			case 3:
+				result = window[functionName](input[0], input[1]);
+			break;
+			case 3:
+				result = window[functionName](input[0], input[1], input[2]);
+			break;
+			case 4:
+				result = window[functionName](input[0], input[1], input[2], input[3]);
+			break;
+		}
+		return result;
+	}
+	return undefined;
+};
 
+testManager.isSimilar = function(a, b) {
+	if(a == b) {
+		if(a === b) {
+			return 2;
+		} else {
+			return 1;
+		}
+	} else {
+		if(typeof a == 'object' && typeof b == 'object') {
+			for(var i in a) {
+				if(i in b && a[i] === b[i]) {
+					continue;
+				}
+				return 0;
+			}
+			for(var j in b) {
+				if(j in a && a[j] === b[j]) {
+					continue;
+				}
+				return 0;
+			}
+			return 2;
+		}
+	}
+	return 0;
+};
+testManager.addMessage = function(type, code) {
+	$('#test-results').append('<div class="message-wrap ' + type + '">' + code + '</div>');
+};
+
+testManager.clearMessages = function() {
+	$('#test-results').html('');
 };
 
 $(document).ready(function() {
--- hometask/tests/tests.json.js
+++ hometask/tests/tests.json.js
@@ -1,22 +1,32 @@
 var testsData = [
 {
-	title: 'Basics 2',
+	title: 'ДЗ по теме JS 2. Массивы',
 	folder: 'task_6',
 	tasks: [{
 		id: 2,
-		title: 'Task 2',
+		title: 'Задача №2. Поломанный reverse',
 		functionName: 'reverse',
 		tests: [
 			{
 				input: [[1,2,3]],
 				output: [3,2,1]
-			},
-			{
-				input: [[null,undefined,'Hello']],
-				output: ['Hello',undefined,null]
+			},{
+				input: [['!','world','Hello']],
+				output: ['Hello','world','!']
 			}
 		]
-		}
-	]
+	},{
+		id: 3,
+		title: 'Задача №3. Поломанный sort',
+		functionName: 'sortSalaries',
+		tests: [{
+				input: [[3100, 890, 1120, 740, 2100,550, 540, 3200,1570, 250]],
+				output: [250,540,550,740,890, 1120, 1570, 2100,3100, 3200]
+			},
+			{
+				input: [[100, 20, 3, 4000]],
+				output: [3,20,100,4000]
+			}]
+		}]
 }
 ];
