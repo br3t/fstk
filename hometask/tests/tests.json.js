ccd95f85f2fdbdf301c581a1166bace01d4fff79
br3t <bret.snk@gmail.com>
2017-10-11 00:31:24 +0300

    More tests

--- hometask/tests/tests.json.js
+++ hometask/tests/tests.json.js
@@ -26,7 +26,47 @@ var testsData = [
 			{
 				input: [[100, 20, 3, 4000]],
 				output: [3,20,100,4000]
-			}]
-		}]
+			}
+		]
+	},{
+		id: 4,
+		title: 'Задача №4. Максимум из минимумов',
+		functionName: 'minimax',
+		tests: [{
+				input: [[3100, 890, 1120, 740, 2100,550, 540, 3200,1570, 250]],
+				output: 890
+			},
+			{
+				input: [[1500, 500, 100, 6000, 70]],
+				output: 500
+			}
+		]
+	},{
+		id: 5,
+		title: 'Задача №5. Поиск квадратов',
+		functionName: 'getSquares',
+		tests: [{
+				input: [[1, 2, 3, 4, 8, 9, 15, 16,34, 36, 38, 24, 25, 26]],
+				output: [1,4,9,16,36,25]
+			},
+			{
+				input: [[121,120,49,48,50]],
+				output: [121,49]
+			}
+		]
+	},{
+		id: 6,
+		title: 'Задача №6. Только уникальные',
+		functionName: 'getSquares',
+		tests: [{
+				input: [['Mike', 'Peter', 'Alex', 'Sonya','Mike', 'Mary', 'Like', 'Alex', 'Abraham', 'Mary']],
+				output: ['Mike','Peter','Alex','Sonya','Mary','Like','Abraham']
+			},
+			{
+				input: [[1,2,2,3,2,3,4,1,4]],
+				output: [1,2,3,4]
+			}
+		]
+	}]
 }
 ];
